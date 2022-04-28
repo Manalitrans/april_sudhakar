f4fbf757a8fdbf8452e8aceb79092574f3b88b65
Manalitrans <manalitrans.reddy@gmail.com>
2022-04-28 15:04:21 +0530

    UPDATED app.java file

--- app.java
+++ app.java
@@ -1,3 +1,7 @@
-HELLO WORLD 
-IAM SUDHAKAR REDDY
-NEW TO DEVOPS
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
